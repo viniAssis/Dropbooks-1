16e96f3eae64a42dd08a43af01982085e33b00eb
viniAssis <39993733+viniAssis@users.noreply.github.com>
2018-09-17 21:58:59 -0300

    Add files via upload

--- src/java/controller/StatusProdutoServlet.java
+++ src/java/controller/StatusProdutoServlet.java
@@ -19,7 +19,7 @@ import modelDAO.UsuarioDAO;
 
 /**
  *
- * @author Elaine
+ * @author Cristiano
  */
 @WebServlet(name = "StatusProdutoServlet", urlPatterns = {"/StatusProdutoServlet"})
 public class StatusProdutoServlet extends HttpServlet {
@@ -39,8 +39,12 @@ public class StatusProdutoServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         
+        int id = Integer.parseInt(request.getParameter("id"));
+         
+        //Produto prod = new ProdutoDAO().getProduto(id);
+         
         // ID do produto
-        prod.setId(1);
+        prod.setId(id);
         
         // altera para o número oposto
         if(prod.getAtivo() == 1){
