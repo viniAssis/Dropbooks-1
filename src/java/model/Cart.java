c5038103f495e02ef4df98e6004069b941840601
Caio Silva <caiomdev@gmail.com>
2018-10-18 01:37:23 -0300

    Atualizando para o uso do HashMap

--- src/java/model/Cart.java
+++ src/java/model/Cart.java
@@ -1,75 +1,48 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import javax.servlet.http.Cookie;
-import model.ItemCart;
-import model.Produto;
 
 public class Cart {
-    private boolean exists = false;
-    private String key, value = "0";
         
-    public HashMap<String, String> AddItemCart(int produto, HashMap<String, String> lista) {
-        
-        if (lista == null) {
-            lista = new HashMap<String, String>();
-        }
+    public HashMap<Integer, Integer> AddItemCart(int produto, HashMap<Integer, Integer> lista) {
 
         // Verificar se o item a ser adicionar, já existe na lista
-        for (int i = 0; i < lista.size(); i++) {
-            int idLista = Integer.parseInt(lista.get(i));
-            if (idLista == produto){
+        /* for (int i = 0; i < lista.size(); i++) {
+            if (lista.containsKey(produto)){
                 exists = true;
-                key = String.valueOf(i);
+                key = i;
             }
-        }
+        } */
 
         // Caso já exista na lista, adicionar 1 na quantidade, ao invés de adicionar um item repetido
-        if (exists) {
-            lista.get(key)
-            itemExistente.setQuantidade(itemExistente.getQuantidade() + 1);
-        } else {
-            ItemCart item = new ItemCart();
-            item.setProduto(produto);
-            item.setQuantidade(1);
-            lista.add(item);
+        int value = 1;
+
+        if (lista.containsKey(produto)) {
+            value = lista.get(produto) + 1;
+        }
+        
+        lista.put(produto, value);
+        
+        return lista;
+    }
+    
+    public HashMap<Integer, Integer> RemoveItemCart(int produto, HashMap<Integer, Integer> lista) {
+        
+        if (lista.containsKey(produto)) {
+            lista.remove(produto);
         }
         
         return lista;
     }
     
-    public HashMap RemoveItemCart(Produto produto, ArrayList lista) {
-        
-        if (lista == null) {
-            lista = new ArrayList();
-        }
+    public HashMap<Integer, Integer> UpdateItemCart(int codLivro, int qtd, HashMap<Integer, Integer> lista) {
 
-        // Verificar se o item a ser adicionar, já existe na lista
         for (int i = 0; i < lista.size(); i++) {
-            int idsLista = ((ItemCart)lista.get(i)).getProduto().getId();
-            if (idsLista == produto.getId()){
-                lista.remove(i);
+            if (lista.get(i) == codLivro){
+                lista.put(i, qtd);
             }
         }
         
         return lista;
-    }
-    
-    public ArrayList UpdateItemCart(int codLivro, int qtd, ArrayList lista) {
-        
-        if (lista == null) {
-            lista = new ArrayList();
-        }
-
-        // Verificar se o item a ser adicionar, já existe na lista
-        for (int i = 0; i < lista.size(); i++) {
-            ItemCart item = ((ItemCart)lista.get(i));
-            if (item.getProduto().getId() == codLivro){
-                item.setQuantidade(qtd);
-            }
-        }
-        
-        return lista;
-    }
+    }   
 }
