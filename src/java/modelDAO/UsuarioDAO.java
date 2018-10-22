b8baa1949141cb9f77ac4b7fa716c8e2f6dfe25d
Caio Silva <caiomdev@gmail.com>
2018-10-22 16:02:41 -0300

    Adicionado função que busca usuário por ID

--- src/java/modelDAO/UsuarioDAO.java
+++ src/java/modelDAO/UsuarioDAO.java
@@ -104,4 +104,47 @@ public class UsuarioDAO {
 
         return resp;
     } 
+    
+    public static Usuario getUserById(int id) {
+        
+        Usuario usuario = new Usuario();
+        
+        try {
+            Connection con = Conecta.getConexao();
+            String sql = "SELECT * FROM usuario WHERE id=?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
+                usuario.setSexo(rs.getString("sexo"));
+                usuario.setDataNascimento(rs.getDate("dataNascimento"));
+                usuario.setId(rs.getInt("id"));
+                usuario.setTipoPessoa(rs.getInt("tipoPessoa"));
+                usuario.setComplemento(rs.getString("complemento"));
+                usuario.setCpf_cnpj(rs.getString("cpf_cnpj"));
+                usuario.setEstado(rs.getString("estado"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setNomeRazao(rs.getString("nomeRazao"));
+                usuario.setTelefone(rs.getString("telefone"));
+                usuario.setNumero(rs.getString("numero"));
+                usuario.setCidade(rs.getString("cidade"));
+                usuario.setBairro(rs.getString("bairro"));
+                usuario.setCep(rs.getString("cep"));
+                usuario.setLogradouro(rs.getString("logradouro"));
+                usuario.setSenha(rs.getString("senha"));
+            }else{
+                usuario = null;
+            }
+            
+            rs.close();
+            ps.close();
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return usuario;
+    }
 }
