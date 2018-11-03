5caa95f8cfd6cd9fb7d5a9e721ce5a4bef4d3d39
Cristiano Morais <devcristianomorais@gmail.com>
2018-11-04 08:47:49 +0900

    Alterando carrossel pelo administrador

--- src/java/model/Mensagem.java
+++ src/java/model/Mensagem.java
@@ -11,5 +11,6 @@ public class Mensagem {
     public static String ERRO_NAO_IDENTIFICADO = "Houve um erro nao identificado na operacao.";
     public static String ERRO_CONEXAO = "Por favor, verifique a sua conexão.";
     public static String EMAIL_ENVIADO = "Seu email foi enviado com sucesso !";
+    public static String BANNER_ENVIADO = "Seu banner foi enviado com sucesso !";
 
 }
--- src/java/modelDAO/Conecta.java
+++ src/java/modelDAO/Conecta.java
@@ -10,7 +10,7 @@ public class Conecta {
             String serverName = "localhost:3306";
             String mydatabase = "dropbooks";
             String username = "root";
-            String password = "root";
+            String password = "";
             String driverName = "com.mysql.jdbc.Driver";
             Class.forName(driverName);
             String url = "jdbc:mysql://" + serverName + "/" + mydatabase + "?allowPublicKeyRetrieval=true&useSSL=false";
